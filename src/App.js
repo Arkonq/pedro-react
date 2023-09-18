aed01d8188cb30693973bd14fc9312c9e07e3b65
Arkonq <arkonq@gmail.com>
2023-09-18 14:01:00 +0600

    lesson-4

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
+import { useState } from 'react';
 import './App.css';
-import User from './User';
-import Planet from './Planet';
 
-function App() {
-  const names = [
-    {name: "Jack", age: 23},
-    {name: "Matt", age: 26},
-    {name: "Rat", age: 41},
-  ];  
-
-  const planets = [
-    { name: "Mercury", isGasPlanet: false },
-    { name: "Venus", isGasPlanet: false },
-    { name: "Earth", isGasPlanet: false },
-    { name: "Mars", isGasPlanet: false },
-    { name: "Jupiter", isGasPlanet: true },
-    { name: "Saturn", isGasPlanet: true },
-    { name: "Uranus", isGasPlanet: true },
-    { name: "Neptune", isGasPlanet: true },
-  ];
+function App() {  
+  const [display, setDisplay] = useState(true);
+  const [count, setCount] = useState(0);
 
   return (
   <div className="App">
-    <h2>Names</h2>
-    {names.map((user, key) => (
-      <User name={user.name} age={user.age} key={key}/>
-    ))}  
-    <h2>Gas Planets</h2>
-    {planets.map((planet, key) => (
-      <Planet name = {planet.name} isGasPlanet = {planet.isGasPlanet} key = {key} />
-    ))}
-    <h2>Not Gas Planets</h2>
-    {planets.map((planet, key) => (
-      <Planet name = {planet.name} isGasPlanet = {!planet.isGasPlanet} key = {key} />
-    ))}
+    <h2 style={{color: display ? "red" : "green"}}>Spoiler text</h2>
+    <button onClick={() => {setDisplay(!display)}}>Show/Hide</button>
+    <h2>Counter App</h2>
+    <p>Num is {count}</p>    
+    <button onClick={() => {setCount(count + 1)}}>Increase</button>
+    <button onClick={() => {setCount(count - 1)}}>Decrease</button>
+    <button onClick={() => {setCount(0)}}>Set to Zero</button>
   </div>
   );
 }
